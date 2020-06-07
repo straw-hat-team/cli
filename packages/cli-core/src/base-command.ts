21acb84d4e0b0de3282e5ea6736af597918a53f5
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2020-06-06 18:06:36 -0700

    fix: fix debugger scope
    affects: @straw-hat/cli-core

--- packages/cli-core/src/base-command.ts
+++ packages/cli-core/src/base-command.ts
@@ -1,8 +1,11 @@
 import Command from '@oclif/command';
 import chalk from 'chalk';
 import ciInfo from 'ci-info';
+import { createDebugger } from './debug';
 
 export abstract class BaseCommand extends Command {
+  debug = createDebugger('base-command');
+
   async init() {
     const ciName = ciInfo.name ?? 'CI';
     this.debug(`${this.config.name}: ${chalk.green(this.config.version)}`);
