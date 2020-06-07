1591a243f3e45940fb9ea9acfe0b68f34add9bcd
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2020-06-06 21:03:40 -0700

    feat: add editorconfig commmand
    affects: @straw-hat/cli-core, @straw-hat/cli

---
+++ packages/cli/src/commands/editorconfig.ts
@@ -0,0 +1,39 @@
+import * as path from 'path';
+import { BaseCommand, Flags } from '@straw-hat/cli-core/dist/base-command';
+import { render } from '@straw-hat/cli-core/dist/template';
+
+const templatePath = path.join(
+  __dirname,
+  '..',
+  '..',
+  'templates',
+  'commands',
+  'editorconfig',
+  '.editorconfig.ejs'
+);
+
+function resolveFileLocation(context: string) {
+  return path.join(context, '.editorconfig');
+}
+
+export class EditorConfigCommand extends BaseCommand {
+  static description = 'Generates the .editorconfig file.';
+
+  static flags = {
+    context: Flags.string({
+      description:
+        'Directory where the .editorconfig file will be created. Defaults to current working directory.',
+    }),
+  };
+
+  async run() {
+    const { flags } = this.parse(EditorConfigCommand);
+    const context = flags.context ?? process.cwd();
+    const destPath = resolveFileLocation(context);
+
+    await render({
+      fromPath: templatePath,
+      destPath,
+    });
+  }
+}
