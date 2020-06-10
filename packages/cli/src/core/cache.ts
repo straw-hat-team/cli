08abf0618e9000354259a558c9dd58407b92ef10
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2020-06-10 02:00:06 -0700

    refactor: remove old core
    affects: @straw-hat/cli

--- packages/cli/src/core/cache.ts
+++
@@ -1,9 +0,0 @@
-export class Cache<K, V> extends Map<K, V> {
-  getOrSet(key: K, callback: (key: K) => V) {
-    if (!this.has(key)) {
-      this.set(key, callback(key));
-    }
-
-    return this.get(key);
-  }
-}
--- packages/cli/src/core/debug.ts
+++
@@ -1,6 +0,0 @@
-import debug from 'debug';
-
-export function createDebugger(...scope: string[]) {
-  const namespace = ['straw-hat', 'cli'].concat(scope).join(':');
-  return debug(namespace);
-}
